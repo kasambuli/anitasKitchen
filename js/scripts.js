7227f0bf4094b6640451261cbf178fdef31d9816
Cynthia Kasambuli <cynthiakasambuli@gmail.com>
2018-02-21 09:12:42 +0300

    add alert to email subscription

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
+  $(".button2").click(function(event) {
+    var emailInput = $("input#email").val()
 
+
+    event.preventDefault();
+    alert(emailInput + "you've successfully subscribed")
+
+  });
+});
